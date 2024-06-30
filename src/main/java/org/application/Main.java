91941752bfdea15f554b75d087b30d190357c83b
CordRafael <rafaelmarins@id.uff.br>
2024-06-30 18:07:25 -0300

    bgl de medir o tempo

--- src/main/java/org/application/Main.java
+++ src/main/java/org/application/Main.java
@@ -1,6 +1,26 @@
 package org.application;
 
-public class Main {
+import entity.Cliente;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente primeiro = new Cliente();
+        Cliente cliente = new Cliente();
+        
+        primeiro.setDestino("sao paulo");
+        
+        List<Cliente> listcliente = new ArrayList<>();
+        for (int i = 0; i < ; i++) {
+            
+        }
+        
+        
+
+        printLn
+
+    }
 
 }
