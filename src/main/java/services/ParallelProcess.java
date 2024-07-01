fe18fa1498f30a4fca51dac17cd14d1c583d53fc
CordRafael <rafaelmarins@id.uff.br>
2024-06-30 22:20:46 -0300

    processo paralelo v1

--- src/main/java/services/ParallelProcess.java
+++ src/main/java/services/ParallelProcess.java
@@ -1,5 +1,88 @@
 package services;
 
+import entity.Cliente;
+import entity.Hotel;
+import entity.Voos;
+import repository.CSVWriting;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParallelProcess {
-    
+
+    public static void processarUsuarios(List<Cliente> clientes, List<Hotel> hoteis, List<Voos> voos, String caminhoHoteis, String caminhoVoos) {
+
+        List<Thread> threads = new ArrayList<>();
+
+
+        for (Cliente cliente : clientes) {
+            Thread thread = new Thread(new Tarefa(cliente, hoteis, voos));
+            threads.add(thread);
+            thread.start();
+        }
+
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        try {
+            CSVWriting.atualizarHoteis(caminhoHoteis, hoteis);
+            CSVWriting.atualizarVoos(caminhoVoos, voos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class Tarefa implements Runnable {
+        private final Cliente cliente;
+        private final List<Hotel> hoteis;
+        private final List<Voos> voos;
+
+        public Tarefa(Cliente cliente, List<Hotel> hoteis, List<Voos> voos) {
+            this.cliente = cliente;
+            this.hoteis = hoteis;
+            this.voos = voos;
+        }
+
+        @Override
+        public void run() {
+            Hotel hotel = ReservaService.buscarHotelMaisBarato(hoteis, cliente.getDestino(), cliente.getEstrelas());
+            List<Voos> voosEncontrados = ReservaService.buscarVoosMaisBaratos(voos, cliente.getOrigem(), cliente.getDestino());
+
+
+            if (hotel == null) {
+                System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " não encontrou hotel disponível");
+                return;
+            }
+
+
+            if (voosEncontrados.isEmpty()) {
+                voosEncontrados = ReservaService.buscarVoosComConexoes(voos, cliente.getOrigem(), cliente.getDestino());
+            }
+
+
+            double custoTotal = hotel.getPreco() + voosEncontrados.stream().mapToDouble(Voos::getPreco).sum();
+            if (custoTotal <= cliente.getDinheiro()) {
+                System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " aceitou o orçamento de R$ " + custoTotal);
+                synchronized (hoteis) {
+                    hotel.setVagas(hotel.getVagas() - 1);
+                }
+                synchronized (voos) {
+                    for (Voos voo : voosEncontrados) {
+                        voo.setQtd_assentos(voo.getQtd_assentos() - 1);
+                    }
+                }
+            } else {
+                System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " não aceitou o orçamento de R$ " + custoTotal);
+            }
+        }
+    }
 }
+
--- src/main/java/services/SerialProcess.java
+++ src/main/java/services/SerialProcess.java
@@ -9,42 +9,46 @@ import java.io.IOException;
 import java.util.List;
 
 public class SerialProcess {
-    public static void processarUsuarios(List<Cliente> clientes, List<Hotel> hoteis, List<Voos> voos, String caminhoHoteis, String caminhoVoos){
-        // Implementação do processamento dos dados
-        for(Cliente cliente : clientes) {
-            Hotel hotel = ReservaService.buscarHotelMaisBarato(hoteis, cliente.getDestino(), cliente.getEstrelas());
-            List<Voos> voosEncontrados = ReservaService.buscarVoosMaisBaratos(voos, cliente.getOrigem(), cliente.getDestino());
 
-            // caso não haja hoteis disponiveis, por ex. falta de vagas ou local indisponivel
-            if(hotel == null){
-                System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " não encontrou hotel disponível");
-                continue;
-            }
 
-            // tratar voos com no maximo 2 conexões
-            if(voosEncontrados.isEmpty()){
-                voosEncontrados = ReservaService.buscarVoosComConexoes(voos, cliente.getOrigem(), cliente.getDestino());
-            }
+        public static void processarUsuarios
+        (List < Cliente > clientes, List < Hotel > hoteis, List < Voos > voos, String caminhoHoteis, String caminhoVoos)
+        {
+            // Implementação do processamento dos dados
+            for (Cliente cliente : clientes) {
+                Hotel hotel = ReservaService.buscarHotelMaisBarato(hoteis, cliente.getDestino(), cliente.getEstrelas());
+                List<Voos> voosEncontrados = ReservaService.buscarVoosMaisBaratos(voos, cliente.getOrigem(), cliente.getDestino());
 
-            // calcular o orçamento do cliente
-            double custoTotal = hotel.getPreco() + voosEncontrados.stream().mapToDouble(Voos::getPreco).sum();
-            if (custoTotal <= cliente.getDinheiro()) {
-                System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " aceitou o orçamento de R$ " + custoTotal);
-                hotel.setVagas(hotel.getVagas() - 1);
-                for (Voos voo : voosEncontrados) {
-                    voo.setQtd_assentos(voo.getQtd_assentos() - 1);
+                // caso não haja hoteis disponiveis, por ex. falta de vagas ou local indisponivel
+                if (hotel == null) {
+                    System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " não encontrou hotel disponível");
+                    continue;
                 }
-            } else {
-                System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " não aceitou o orçamento de R$ " + custoTotal);
+
+                // tratar voos com no maximo 2 conexões
+                if (voosEncontrados.isEmpty()) {
+                    voosEncontrados = ReservaService.buscarVoosComConexoes(voos, cliente.getOrigem(), cliente.getDestino());
+                }
+
+                // calcular o orçamento do cliente
+                double custoTotal = hotel.getPreco() + voosEncontrados.stream().mapToDouble(Voos::getPreco).sum();
+                if (custoTotal <= cliente.getDinheiro()) {
+                    System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " aceitou o orçamento de R$ " + custoTotal);
+                    hotel.setVagas(hotel.getVagas() - 1);
+                    for (Voos voo : voosEncontrados) {
+                        voo.setQtd_assentos(voo.getQtd_assentos() - 1);
+                    }
+                } else {
+                    System.out.println("Cliente " + cliente.getNome().split(" ")[1] + " não aceitou o orçamento de R$ " + custoTotal);
+                }
             }
-        }
 
-        try{
-            CSVWriting.atualizarHoteis(caminhoHoteis, hoteis);
-            CSVWriting.atualizarVoos(caminhoVoos, voos);
-        }catch (IOException e){
-            e.printStackTrace();
+            try {
+                CSVWriting.atualizarHoteis(caminhoHoteis, hoteis);
+                CSVWriting.atualizarVoos(caminhoVoos, voos);
+            } catch (IOException e) {
+                e.printStackTrace();
 
+            }
         }
     }
-}
